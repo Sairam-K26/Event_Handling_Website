9a7db5bcc4c4445e43202abd1b0c2eec0906f200
Sairam . K <122731540+Sairam-K26@users.noreply.github.com>
2023-09-08 14:03:47 +0530

    Update settings.py

--- EventManagementSystem/settings.py
+++ EventManagementSystem/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6%ur)wpoz&h2glf8htrh+z*pe1@0!flwsczv#t3(5om46($ufy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
